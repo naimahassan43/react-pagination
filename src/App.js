7da6166df2d28ea5260f32ba651a5f94d3f3cbe8
Naima Hassan <naima.hassan434@gmail.com>
2022-06-04 16:19:43 +0100

    Prev/Next Buttons

--- src/App.js
+++ src/App.js
@@ -17,6 +17,24 @@ function App() {
   const handlePage = (index) => {
     setPage(index);
   };
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
+  };
+  const prevPage = () => {
+      setPage((oldPage) => {
+        let prevPage = oldPage - 1;
+        if (prevPage <0 ) {
+          prevPage = data.length - 1;
+        }
+        return prevPage;
+      });
+  };
 
   return (
     <main>
@@ -32,6 +50,10 @@ function App() {
         </div>
         {!loading && (
           <div className="btn-container">
+            <button className="prev-btn" onClick={prevPage}>
+              prev
+            </button>
+
             {data.map((item, index) => {
               return (
                 <button
@@ -43,6 +65,10 @@ function App() {
                 </button>
               );
             })}
+
+            <button className="next-btn" onClick={nextPage}>
+              next
+            </button>
           </div>
         )}
       </section>
